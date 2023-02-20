407060b638becd953245f4a2db2e1deb8d61b4b5
YUN <rjrjsktl@gmail.com>
2023-02-20 19:30:04 +0900

    Revert "삭제함"
    This reverts commit f95b08c6930475c72895caa6a000f16805919730.

--- src/edu/kh/git/practice/GitPracticeRun.java
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -3,8 +3,8 @@ package edu.kh.git.practice;
 public class GitPracticeRun {
 
 	public static void main(String[] args) {
-		System.out.println("안녕하세요~!");
-		
+		// TODO Auto-generated method stub
+
 	}
 
 }
