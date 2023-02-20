9b39367c36bf40fcde550fb0d08e4fd2602e10f0
YUN <rjrjsktl@gmail.com>
2023-02-20 20:16:52 +0900

    연결 완료

--- src/edu/kh/git/practice/GitPracticeRun.java
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -5,6 +5,8 @@ public class GitPracticeRun {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요~!");
 		System.out.println("Git 연습용 파일입니다");
+		
+		System.out.println("github 연동 완료");
 	}
 
 }
